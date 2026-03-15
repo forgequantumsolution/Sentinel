618a524a84e4408993bcf26ca0b1642e8734241b
Zeeshan Nasir <z.nasir@preventionatsea.com>
2026-03-15 23:25:19 +0530

    mig added

---
+++ Infrastructure/Migrations/20260315175231_GraphFolder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class GraphFolder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ActionObjectId",
+                table: "GraphConfigs",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GraphConfigs_ActionObjectId",
+                table: "GraphConfigs",
+                column: "ActionObjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GraphConfigs_ActionObjects_ActionObjectId",
+                table: "GraphConfigs",
+                column: "ActionObjectId",
+                principalTable: "ActionObjects",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GraphConfigs_ActionObjects_ActionObjectId",
+                table: "GraphConfigs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GraphConfigs_ActionObjectId",
+                table: "GraphConfigs");
+
+            migrationBuilder.DropColumn(
+                name: "ActionObjectId",
+                table: "GraphConfigs");
+        }
+    }
+}
